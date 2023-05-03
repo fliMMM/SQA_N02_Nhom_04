8d24a2dcdecf9d8b19b2b1f0790bd34a3cc194c3
fliMMM <bachkame@gmail.com>
2023-05-03 19:31:47 +0700

    them baseurl api

--- app/src/api/axiosClient.tsx
+++ app/src/api/axiosClient.tsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const devBaseUrl = "http://localhost:5555/api/v1/";
+const prodBaseUrl = "https://sore-teal-kangaroo-slip.cyclic.app/api/v1/";
+
+
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5555/api/v1/",
+  baseURL: prodBaseUrl,
   timeout:10000,
   headers: {
     'Content-Type': 'application/json',
